dad6873ba8bdc07efddcde26735a580249dbbb3b
Taletex <alessandromessina95@hotmail.com>
2020-07-31 01:09:41 +0200

    Implemented session CRUD in UI

--- WebApp/src/app/components/training/training.component.ts
+++ WebApp/src/app/components/training/training.component.ts
@@ -11,16 +11,23 @@ import * as _ from "lodash";
 export class TrainingComponent implements OnInit {
 
   public training;
+  public defaultSession;
+  public copiedSession;
   public defaultExercise;
   public copiedExercise;
   public defaultSeries;
   public copiedSeries;
+  public activeSession;
 
   constructor(public router: Router) {
+    // TODO: get last id for sessions
+    this.defaultSession = {id: 10000001, name: "", comment: "", exercises: [{id: "12345678", name: "deadlift", variant: {name: "standard", intensityCoefficient: 1}, series: [{seriesNumber: 1, repNumber: 1, weight: 50, measure: "%", rest: "90"}]} ]};
+    this.copiedSession = {id: 10000001, name: "", comment: "", exercises: [{id: "12345678", name: "deadlift", variant: {name: "standard", intensityCoefficient: 1}, series: [{seriesNumber: 1, repNumber: 1, weight: 50, measure: "%", rest: "90"}]} ]};
     this.defaultExercise = {id: "12345678", name: "deadlift", variant: {name: "standard", intensityCoefficient: 1}, series: [{seriesNumber: 1, repNumber: 1, weight: 50, measure: "%", rest: "90"}]};
     this.copiedExercise = {id: "12345678", name: "deadlift", variant: {name: "standard", intensityCoefficient: 1}, series: [{seriesNumber: 1, repNumber: 1, weight: 50, measure: "%", rest: "90"}]};
     this.defaultSeries = {seriesNumber: 1, repNumber: 1, weight: 50, measure: "%", rest: "90"};
     this.copiedSeries = {seriesNumber: 1, repNumber: 1, weight: 50, measure: "%", rest: "90"};
+    this.activeSession = 1;
 
     let trainingList = ((data as any).default);
     let trainingId = (this.router.url).split('/')[2];
@@ -39,6 +46,9 @@ export class TrainingComponent implements OnInit {
     
   }
 
+  // TODO: quando si compie una di queste azioni mostrare un toastr che ti dice l'azione se andataa buon fine o meno
+
+  /* SERIES FUNCTIONS */
   pushSeries(exercise: any) {
     if (exercise && exercise.series != null) {
       exercise.series.push(_.cloneDeep(this.defaultSeries));
@@ -48,7 +58,7 @@ export class TrainingComponent implements OnInit {
   }
 
   resetSeries(exercise: any, index: number) {
-    if (exercise && exercise.series != null && index <exercise.series.length) {
+    if (exercise && exercise.series != null && index < exercise.series.length) {
       exercise.series[index] = _.cloneDeep(this.defaultSeries);
     } else {
       console.log('ERROR: resetting series of index ' + index);
@@ -56,7 +66,7 @@ export class TrainingComponent implements OnInit {
   }
 
   deleteSeries(exercise: any, index: number) {
-    if (exercise && exercise.series != null && index <exercise.series.length) {
+    if (exercise && exercise.series != null && index < exercise.series.length) {
       exercise.series.splice(index, 1);
     } else {
       console.log('ERROR: removing series of index ' + index);
@@ -64,7 +74,7 @@ export class TrainingComponent implements OnInit {
   }
 
   copySeries(exercise: any, index: number) {
-    if (exercise && exercise.series != null && index <exercise.series.length) {
+    if (exercise && exercise.series != null && index < exercise.series.length) {
       this.copiedSeries = _.cloneDeep(exercise.series[index]);
     } else {
       console.log('ERROR: copying series of index ' + index);
@@ -72,23 +82,24 @@ export class TrainingComponent implements OnInit {
   }
 
   pasteSeries(exercise: any, index: number) {
-    if (exercise && exercise.series != null && index <exercise.series.length) {
+    if (exercise && exercise.series != null && index < exercise.series.length) {
       exercise.series[index] = _.cloneDeep(this.copiedSeries);
     } else {
       console.log('ERROR: pasting series of index ' + index);
     }
   }
 
+  /* EXERCISES FUNCTIONS */
   pushExercise(training: any) {
     if (training && training.exercises != null) {
-      training.exercises.push(this.defaultExercise);
+      training.exercises.push(_.cloneDeep(this.defaultExercise));
     } else {
       console.log('Error: "exercises" is not defined');
     }
   }
 
   resetExercise(training: any, index: number) {
-    if (training && training.exercises != null && index <training.exercises.length) {
+    if (training && training.exercises != null && index < training.exercises.length) {
       training.exercises[index] = _.cloneDeep(this.defaultExercise);
     } else {
       console.log('ERROR: resetting exercise of index ' + index);
@@ -96,7 +107,7 @@ export class TrainingComponent implements OnInit {
   }
 
   deleteExercise(training: any, index: number) {
-    if (training && training.exercises != null && index <training.exercises.length) {
+    if (training && training.exercises != null && index < training.exercises.length) {
       training.exercises.splice(index, 1);
     } else {
       console.log('ERROR: removing exercise of index ' + index);
@@ -104,7 +115,7 @@ export class TrainingComponent implements OnInit {
   }
 
   copyExercise(training: any, index: number) {
-    if (training && training.exercises != null && index <training.exercises.length) {
+    if (training && training.exercises != null && index < training.exercises.length) {
       this.copiedExercise = _.cloneDeep(training.exercises[index]);
     } else {
       console.log('ERROR: copying exercise of index ' + index);
@@ -112,10 +123,63 @@ export class TrainingComponent implements OnInit {
   }
 
   pasteExercise(training: any, index: number) {
-    if (training && training.exercises != null && index <training.exercises.length) {
+    if (training && training.exercises != null && index < training.exercises.length) {
       training.exercises[index] = _.cloneDeep(this.copiedExercise);
     } else {
       console.log('ERROR: pasting exercise of index ' + index);
     }
   }
+
+  /* SESSIONS FUNCTIONS */
+  pushSession(event: MouseEvent, week: any) {
+    if (week && week.sessions != null) {
+      week.sessions.push(_.cloneDeep(this.defaultSession));
+    } else {
+      console.log('Error: "sessions" is not defined');
+    }
+    event.preventDefault();
+  }
+
+  resetSession(event: MouseEvent, week: any, index: number) {
+    if (week && week.sessions != null && index < week.sessions.length) {
+      week.sessions[index] = _.cloneDeep(this.defaultSession);
+    } else {
+      console.log('ERROR: resetting session of index ' + index);
+    }
+    event.preventDefault();
+    event.stopImmediatePropagation();
+  }
+
+  deleteSession(event: MouseEvent, week: any, index: number) {
+    if (week && week.sessions != null && index < week.sessions.length) {
+      week.sessions.splice(index, 1);
+      this.activeSession = 1;
+    } else {
+      console.log('ERROR: removing session of index ' + index);
+    }
+    event.preventDefault();
+    event.stopImmediatePropagation();
+  }
+
+  copySession(event: MouseEvent, week: any, index: number) {
+    if (week && week.sessions != null && index < week.sessions.length) {
+      this.copiedSession = _.cloneDeep(week.sessions[index]);
+    } else {
+      console.log('ERROR: copying session of index ' + index);
+    }
+    event.preventDefault();
+    event.stopImmediatePropagation();
+  }
+
+  pasteSession(event: MouseEvent, week: any, index: number) {
+    if (week && week.sessions != null && index < week.sessions.length) {
+      week.sessions[index] = _.cloneDeep(this.copiedSession);
+    } else {
+      console.log('ERROR: pasting session of index ' + index);
+    } 
+    event.preventDefault();
+    event.stopImmediatePropagation();
+  }
+
+
 }
