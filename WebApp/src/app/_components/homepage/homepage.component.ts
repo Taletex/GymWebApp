8c082a44c84171a6ed10064ecaf99b6b6c15e77e
Taletex <alessandromessina95@hotmail.com>
2020-09-24 00:45:07 +0200

    Updated homepage

--- WebApp/src/app/_components/homepage/homepage.component.ts
+++ WebApp/src/app/_components/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToSection(section: string) {
+    this.router.navigate([section]);
   }
 
 }
