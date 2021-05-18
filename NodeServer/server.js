3d235a7ab6803a2b86645f6e146d530d6a6076e6
Alessandro Messina <alessandromessina95@hotmail.com>
2021-05-18 13:31:30 +0200

    mf

--- NodeServer/server.js
+++ NodeServer/server.js
@@ -7,6 +7,8 @@ const fs = require('fs');
 const errorHandler = require('src/_middleware/error-handler');
 const _ = require('lodash');
 const fileManager = require('src/_helpers/fileManager');
+var httpServer;
+
 
 /* EXPRESS INITIALIZATION */
 // create express app
@@ -21,11 +23,30 @@ app.use(cookieParser());
 app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
 
 
-/* SOCKET IO INITIALIZATION */
+/* HTTP/S SERVER INITIALIZATION */
 var clientSocketList = [];
 let socketSize = 0;
-const httpServer = require('http').Server(app);
-const io = require('socket.io')(httpServer, {cors: {origin: '*', credentials: true}});       // TODO: allow cors request but with credentials!
+
+// set https or http basing on NODE_ENV
+if (process.env.NODE_ENV === 'production') {
+    try {
+        httpServer = require('https').createServer({
+            key: fs.readFileSync('server.key'),
+            cert: fs.readFileSync('server.cert')
+        }, app)
+        console.log("Setting Https server");
+    } catch {
+        httpServer = require('http').Server(app);
+        console.log("Setting Http server");
+    }
+} else {
+    httpServer = require('http').Server(app);
+    console.log("Setting Http server");
+}
+
+
+/* SOCKET IO INITIALIZATION */
+const io = require('socket.io')(httpServer, { cors: { origin: '*', credentials: true } });       // TODO: allow cors request but with credentials!
 io.on('connection', (socket) => {
     let socketUserId = socket.client.id;
     socketSize++;
@@ -34,25 +55,27 @@ io.on('connection', (socket) => {
     // When socket disconnects, remove from clientSocketList the element associated (using the socketId)
     socket.once("disconnect", (reason) => {
         socketSize--;
-        _.remove(clientSocketList, function(s) { return s.socketId == socket.id; });
-        console.log("User " + socketUserId + " disconnected with reason '" + reason + "' (" + socketSize + " users connected).");  
+        _.remove(clientSocketList, function (s) { return s.socketId == socket.id; });
+        console.log("User " + socketUserId + " disconnected with reason '" + reason + "' (" + socketSize + " users connected).");
     });
 
     // When client loads the account-service, accountInit event is emitted and used for update socket informations
     socket.once("accountInit", (ids) => {
         socket.accountId = ids.accountId | "";
-        clientSocketList.push({socketId: socket.id, socketUserId: socketUserId, accountId: ids.accountId, userId: ids.userId});
+        clientSocketList.push({ socketId: socket.id, socketUserId: socketUserId, accountId: ids.accountId, userId: ids.userId });
     })
 });
 
 
+/* FILE HOSTING INITIALIZATION */
 // Serve all static files  inside files directory.
 if (!fs.existsSync(fileManager.fileBaseDir))
     fs.mkdirSync(fileManager.fileBaseDir);
 app.use(fileManager.fileBaseDir, express.static(__dirname + fileManager.fileBaseDir));
 
+
 /* ROUTES INITIALIZATION */
-app.get('/', (req, res) => { res.json({ "message": "Welcome to GymWebApp database application." }); });
+app.get('/', (req, res) => { res.json({ "message": "Welcome to GymWebApp Nodeserver" }); });
 app.use('/trainings', require('src/trainings/training.controller')(io, clientSocketList));
 app.use('/exercises', require('src/exercises/exercise.controller'));
 app.use('/users', require('src/users/user.controller')(io, clientSocketList));
@@ -65,10 +88,9 @@ app.use('/api-docs', require('src/_helpers/swagger'));
 app.use(errorHandler);
 
 
-
 /* START SERVER */
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 const hostname = '0.0.0.0';
 httpServer.listen(port, () => {
-    console.log("Http Server listening on port " + port);
+    console.log("Server listening on port " + port + (process.env.NODE_ENV === 'production' ? ' (production environment)' : ' (development environment)'));
 })
--- WebApp/src/environments/environment.prod.ts
+++ WebApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://www.mytrainingplatform.it:4000', //Nota: bisogna usare questo e non l'ip perchè altrimenti il refresh quando si accede usando il domain non funziona e da errore di cross-site nell'header http!
-  socketConfig: { url: 'http://www.mytrainingplatform.it:4000', options: {} }
+  apiUrl: 'https://www.mytrainingplatform.it:4000', //Nota: bisogna usare questo e non l'ip perchè altrimenti il refresh quando si accede usando il domain non funziona e da errore di cross-site nell'header http!
+  socketConfig: { url: 'https://www.mytrainingplatform.it:4000', options: {} }
 
 };
